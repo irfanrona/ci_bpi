f53eb975f48740f145ba3dfb0b73e7e97dcc96d4
Irfan Rona Setiadi <chipunk@hackermail.com>
2020-09-17 10:23:22 +0700

    Update config.php
    fix: change base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/src';
+$config['base_url'] = 'http://localhost/ci_bpi';
 
 /*
 |--------------------------------------------------------------------------
